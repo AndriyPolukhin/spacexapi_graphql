942450ce2cca27974964ebe5a8304b6cb885c624
AndriyPolukhin <andriy.polukhin@gmail.com>
2019-04-19 13:58:12 +0300

    app finished

---
+++ public/precache-manifest.00f88ebed85afe20ea33ae618f656737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "097edbfbb3309ee28875",
+    "url": "/static/css/main.40e416a0.chunk.css"
+  },
+  {
+    "revision": "097edbfbb3309ee28875",
+    "url": "/static/js/main.7bd8e3b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bfdb13267986d2c5c22",
+    "url": "/static/js/2.eb69afd3.chunk.js"
+  },
+  {
+    "revision": "8a8cfb1945f454a980d7496f5afa7a18",
+    "url": "/index.html"
+  }
+];
